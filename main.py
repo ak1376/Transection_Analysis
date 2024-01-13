86b2fa7246611f3995dfe0149a7259361ce836fb
Ananya Kapoor <akapoor@uoregon.edu>
2024-01-13 12:51:43 -0800

    Analysis of one day. Using the visualizer in the utils folder.

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ import numpy as np
 import torch
 import sys
 import os
-os.chdir('/home/akapoor/Dropbox (University of Oregon)/Kapoor_Ananya/01_Projects/01_b_Canary_SSL/TweetyCLR_old_version/USA_5207_Analysis/')
+os.chdir('/home/akapoor/Dropbox (University of Oregon)/Kapoor_Ananya/01_Projects/01_b_Canary_SSL/Transection_Analysis/')
 # sys.path.append('/home/akapoor/Dropbox (University of Oregon)/Kapoor_Ananya/01_Projects/01_b_Canary_SSL/TweetyCLR/USA_5207_Analysis/')
-from util import WavtoSpec, Tweetyclr
+from util import WavtoSpec, Tweetyclr, DataPlotter
 import pickle
 import umap
 import matplotlib.pyplot as plt
+from PIL import Image
+import base64
+import io
+from io import BytesIO
+from bokeh.plotting import figure, show, output_file, save
+from bokeh.models import HoverTool, ColumnDataSource
+import pandas as pd
+import matplotlib.cm as cm
+
 
 # TODO: Include capacity for colorizing by multiple days and loading data from multiple days. 
 
@@ -33,7 +42,7 @@ dest_dir = '/home/akapoor/Dropbox (University of Oregon)/AK_RHV_Analysis/USA5323
 analysis_path = '/home/akapoor/Dropbox (University of Oregon)/AK_RHV_Analysis/USA5323/UMAP_Analysis/'
 
 # Parameters we set
-num_spec = 10 # The number of spectrograms for analysis 
+num_spec = 37 # The number of spectrograms for analysis 
 window_size = 100 # The window size for UMAP 
 stride = 10 # Stride size for analysis 
 
@@ -150,12 +159,17 @@ stacked_labels_for_window = np.squeeze(stacked_labels_for_window)
 stacked_window_times = np.stack(stacked_window_times, axis = 0)
 
 # For each mini-spectrogram, find the average color across all unique syllables
-mean_colors_per_minispec = np.zeros((stacked_labels_for_window.shape[0], 3))
-for i in np.arange(stacked_labels_for_window.shape[0]):
-    list_of_colors_for_row = [category_colors[x] for x in stacked_labels_for_window[i,:]]
-    all_colors_in_minispec = np.array(list_of_colors_for_row)
-    mean_color = np.mean(all_colors_in_minispec, axis = 0)
-    mean_colors_per_minispec[i,:] = mean_color
+# mean_colors_per_minispec = np.zeros((stacked_labels_for_window.shape[0], 3))
+# for i in np.arange(stacked_labels_for_window.shape[0]):
+#     list_of_colors_for_row = [category_colors[x] for x in stacked_labels_for_window[i,:]]
+#     all_colors_in_minispec = np.array(list_of_colors_for_row)
+#     mean_color = np.mean(all_colors_in_minispec, axis = 0)
+#     mean_colors_per_minispec[i,:] = mean_color
+
+# Creating mean_colors_per_minispec but colorized by time (i.e. slice number). This is good if we have just one day of song to decompose. 
+n = stacked_windows.shape[0]
+mean_colors_per_minispec = cm.plasma(np.linspace(0, 1, n))  # Generate n distinct colors
+
 
 # DO UMAP
 reducer = umap.UMAP()
@@ -169,6 +183,91 @@ plt.title("UMAP Decomposition")
 plt.show()
 
 np.save(f'{folder_name}/embedding.npy', embedding)
+
+# =============================================================================
+# Bokeh Plotting
+# =============================================================================
+
+def embeddable_image(data):
+    data = (data.squeeze() * 255).astype(np.uint8)
+    # convert to uint8
+    data = np.uint8(data)
+    image = Image.fromarray(data)
+    image = image.rotate(90, expand=True) 
+    image = image.convert('RGB')
+    # show PIL image
+    im_file = BytesIO()
+    img_save = image.save(im_file, format='PNG')
+    im_bytes = im_file.getvalue()
+
+    img_str = "data:image/png;base64," + base64.b64encode(im_bytes).decode()
+    return img_str
+
+
+def get_images(list_of_images):
+    return list(map(embeddable_image, list_of_images))
+
+
+def plot_UMAP_embedding(embedding, mean_colors_per_minispec, image_paths, filepath_name, saveflag = False):
+
+    # Specify an HTML file to save the Bokeh image to.
+    # output_file(filename=f'{self.folder_name}Plots/{filename_val}.html')
+    output_file(filename = f'{filepath_name}')
+
+    # Convert the UMAP embedding to a Pandas Dataframe
+    spec_df = pd.DataFrame(embedding, columns=('x', 'y'))
+
+
+    # Create a ColumnDataSource from the data. This contains the UMAP embedding components and the mean colors per mini-spectrogram
+    source = ColumnDataSource(data=dict(x = embedding[:,0], y = embedding[:,1], colors=mean_colors_per_minispec))
+
+
+    # Create a figure and add a scatter plot
+    p = figure(width=800, height=600, tools=('pan, box_zoom, hover, reset'))
+    p.scatter(x='x', y='y', size = 7, color = 'colors', source=source)
+
+    hover = p.select(dict(type=HoverTool))
+    hover.tooltips = """
+        <div>
+            <h3>@x, @y</h3>
+            <div>
+                <img
+                    src="@image" height="100" alt="@image" width="100"
+                    style="float: left; margin: 0px 15px 15px 0px;"
+                    border="2"
+                ></img>
+            </div>
+        </div>
+    """
+
+    p.add_tools(HoverTool(tooltips="""
+    """))
+    
+    # Set the image path for each data point
+    source.data['image'] = image_paths
+    # source.data['image'] = []
+    # for i in np.arange(spec_df.shape[0]):
+    #     source.data['image'].append(f'{self.folder_name}/Plots/Window_Plots/Window_{i}.png')
+
+
+    save(p)
+    show(p)
+
+
+list_of_images = []
+stacked_windows_plotting = stacked_windows.copy()
+stacked_windows_plotting.shape = (stacked_windows.shape[0], 1, 100, 151)
+
+
+for i in np.arange(stacked_windows_plotting.shape[0]):
+    data = stacked_windows_plotting[i,:,:,:]
+    list_of_images.append(data)
+
+
+embeddable_images = get_images(list_of_images)
+
+plot_UMAP_embedding(embedding, mean_colors_per_minispec,embeddable_images, f'{folder_name}/Plots/UMAP_analysis.html', saveflag = True)
+
 
 # Now let's set up the data structure for the visualizer 
 
